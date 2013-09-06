56b0197c6e480dd40b001236cdc120ce05a885e0
Karl Palsson <karlp@remake.is>
2013-09-06 17:53:28 +0000

    Properly use command arguments to run this sanely

--- load_publisher.py
+++ load_publisher.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import argparse
 import logging
+import os
 import beem.load
 
 logging.basicConfig(level=logging.INFO)
@@ -11,13 +13,42 @@ def custom_make_topic(seq):
 def custom_make_payload(seq, size):
     return "Message %d was meant to be %d bytes long hehe" % (seq, size)
 
-if __name__ == "__main__":
-    ts = beem.load.TrackingSender("localhost", 1883, "karlos1")
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="""
+        Publish a stream of messages and capture statistics on their timing
+        """)
+
+    parser.add_argument(
+        "-c", "--clientid", default="beem.loadr-%d" % os.getpid(),
+        help="Set the client id of the publisher, can be useful for acls")
+    parser.add_argument(
+        "-H", "--host", default="localhost",
+        help="MQTT host to connect to")
+    parser.add_argument(
+        "-p", "--port", type=int, default=1883,
+        help="Port for remote MQTT host")
+    parser.add_argument(
+        "-q", "--qos", type=int, choices=[0, 1, 2],
+        help="set the mqtt qos for messages published", default=1)
+    parser.add_argument(
+        "-n", "--msg_count", type=int, default=10,
+        help="How many messages to send")
+    parser.add_argument(
+        "-s", "--msg_size", type=int, default=100,
+        help="""Size of messages to send. This will be gaussian at (x, x/20)
+unless the make_payload method is overridden""")
+
+
+    options = parser.parse_args()
+
+    ts = beem.load.TrackingSender(options.host, options.port, options.clientid)
     # Provide a custom topic generator
     #ts.make_topic = custom_make_topic
     # Or a custom payload generator
     #ts.make_payload = custom_make_payload
-    ts.run(100, 50)
+    ts.run(options.msg_count, options.msg_size, options.qos)
     stats = ts.stats()
     print("Message succes rate: %.2f%% (%d/%d messages)"
         % (100 * stats["rate_ok"], stats["count_ok"], stats["count_total"]))
@@ -25,3 +56,7 @@ if __name__ == "__main__":
     print("Message timing stddev %.2f ms" % stats["time_stddev"])
     print("Message timing min    %.2f ms" % stats["time_min"])
     print("Message timing max    %.2f ms" % stats["time_max"])
+
+
+if __name__ == "__main__":
+    main()
